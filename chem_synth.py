f2eaee621e1a816771c8be21b7b48ede1f3c42f8
dzleidig <dylan.leidig@strateos.com>
2020-03-30 11:36:06 -0700

    - Add default values for fields - Fix protocol name so it can be uploaded as a package

--- chem_synth.py
+++ chem_synth.py
@@ -1,9 +1,9 @@
 def chem_synth(protocol, params):
     source_well = params["reagent_details"][0]["reagent_container"]
-    protocol.spin(source_well, "5:g", "3:minutes")
+    protocol.spin(source_well, "500:g", "3:minutes")
 
 
 if __name__ == '__main__':
     from autoprotocol.harness import run
 
-    run(chem_synth, "chem_synth")
+    run(chem_synth, "ChemicalSynthesis")
